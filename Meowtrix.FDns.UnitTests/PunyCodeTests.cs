ca507c17faf8f12bb203c86e525041ae7b0b86ca
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-10-15 02:59:35 +0800

    Add test for trivial cases

---
+++ Meowtrix.FDns.UnitTests/PunyCodeTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Meowtrix.FDns.UnitTests
+{
+    public class PunyCodeTests
+    {
+        public static IEnumerable<object[]> TestData
+        {
+            get
+            {
+                yield return new object[] { "", "" };
+                yield return new object[] { "abc", "abc" };
+                yield return new object[] { "0123456789", "0123456789" };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void TestEncoding(string raw, string expected)
+        {
+            Assert.Equal(expected, PunyCode.EncodeToString(raw));
+        }
+    }
+}

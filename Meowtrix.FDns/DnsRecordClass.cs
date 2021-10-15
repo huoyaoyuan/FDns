caea4845a70802e8f63c45b76c4dad2760e68917
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-10-15 23:38:23 +0800

    Add enum definition for DNS records

---
+++ Meowtrix.FDns/DnsRecordClass.cs
@@ -0,0 +1,20 @@
+﻿namespace Meowtrix.FDns
+{
+    // https://datatracker.ietf.org/doc/html/rfc1035#section-3.2.4
+
+    public enum DnsRecordClass : short
+    {
+        /// <summary>
+        /// The Internet
+        /// </summary>
+        IN = 1,
+        /// <summary>
+        /// The CHAOS class
+        /// </summary>
+        CH = 3,
+        /// <summary>
+        /// Hesiod
+        /// </summary>
+        HS = 4,
+    }
+}
---
+++ Meowtrix.FDns/DomainType.cs
@@ -0,0 +1,33 @@
+﻿namespace Meowtrix.FDns
+{
+    // https://datatracker.ietf.org/doc/html/rfc1035#section-3.2.2
+    // https://datatracker.ietf.org/doc/html/rfc3596#section-2.1
+
+    public enum DomainType : short
+    {
+        /// <summary>
+        /// host address
+        /// </summary>
+        A = 1,
+        /// <summary>
+        /// Authoritative name server
+        /// </summary>
+        NS = 2,
+        /// <summary>
+        /// Canonical name for an alias
+        /// </summary>
+        CNAME = 5,
+        /// <summary>
+        /// Mail exchange
+        /// </summary>
+        MX = 15,
+        /// <summary>
+        /// Text strings
+        /// </summary>
+        TXT = 16,
+        /// <summary>
+        /// IPv6 host address
+        /// </summary>
+        AAAA = 28,
+    }
+}
